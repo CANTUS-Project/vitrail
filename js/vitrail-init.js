65ce8d17e3718f9e27e8fa5a2e96335e5393331a
Christopher Antila <christopher@antila.ca>
2015-08-17 18:18:55 -0400

    Rename: vitrail.js --> vitrail.src.js
    This means the source file is clearly marked as such, and the compiled/minified
    versions will be available with the "vitrail.js" filename. This allows importing
    Vitrail as "vitrail" rather than as "vitrail.compiled" or similar.
    
    Working toward #16

--- js/vitrail-init.js
+++ js/vitrail-init.js
@@ -3,7 +3,7 @@
 
 
 // This is written in straight-up ECMAScript 5, bro.
-define(["react", "vitrail.compiled"], function(React, Vitrail) {
+define(["react", "vitrail"], function(React, Vitrail) {
     React.render(
         React.createElement(Vitrail.SearchForm,
                             {rootUrl: "http://localhost:8888/"}),
