9635e9cb317f32bbd876a3cbba1b31e63437e31e
Christopher Antila <christopher@antila.ca>
2015-08-18 17:55:11 -0400

    New: copyright headers in every worthy file.
    Closes #20

--- __tests__/test_Result.js
+++ __tests__/test_Result.js
@@ -1,4 +1,27 @@
-// tests/test_Result.js
+// -*- coding: utf-8 -*-
+//-------------------------------------------------------------------------------------------------
+// Program Name:           vitrail
+// Program Description:    HTML/CSS/JavaScript user agent for the Cantus API.
+//
+// Filename:               __tests__/test_Result.js
+// Purpose:                Tests for the "Result" React component.
+//
+// Copyright (C) 2015 Christopher Antila
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//-------------------------------------------------------------------------------------------------
+
 
 jest.dontMock("../js/vitrail");
 
--- js/vitrail-init.js
+++ js/vitrail-init.js
@@ -1,5 +1,26 @@
-// vitrail-init.js
-// Copyright 2015 Christopher Antila
+// -*- coding: utf-8 -*-
+//-------------------------------------------------------------------------------------------------
+// Program Name:           vitrail
+// Program Description:    HTML/CSS/JavaScript user agent for the Cantus API.
+//
+// Filename:               js/vitrail-init.js
+// Purpose:                Use RequireJS and React to initialize the Vitrail app for browsers.
+//
+// Copyright (C) 2015 Christopher Antila
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//-------------------------------------------------------------------------------------------------
 
 
 // This is written in straight-up ECMAScript 5, bro.
